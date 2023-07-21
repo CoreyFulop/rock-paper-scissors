40889756b99598f2fd9de8434fed8038664aaa43
Corey Fulop <corey.fulop@gmail.com>
2023-07-21 16:02:20 +0100

    Complete playRound function is js
    This function returns the outcome of a round of the game as a string

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,17 @@ const shapes = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
     return shapes[Math.floor(Math.random() * 3)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    const playerSelectionTitleCase = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if ((playerSelectionTitleCase == "Rock" || computerSelection == "Rock") && (playerSelectionTitleCase == "Scissors" || computerSelection == "Scissors")) {
+        return (playerSelectionTitleCase == "Rock") ? "You win! Rock beats Scissors" : "You lose! Rock beats Scissors";
+    } else if ((playerSelectionTitleCase == "Scissors" || computerSelection == "Scissors") && (playerSelectionTitleCase == "Paper" || computerSelection == "Paper")) {
+        return (playerSelectionTitleCase == "Scissors") ? "You win! Scissors beats Paper" : "You lose! Scissors beats Paper";
+    } else if ((playerSelectionTitleCase == "Paper" || computerSelection == "Paper") && (playerSelectionTitleCase == "Rock" || computerSelection == "Rock")) {
+        return (playerSelectionTitleCase == "Paper") ? "You win! Paper beats Rock" : "You lose! Paper beats Rock";
+    } else {
+        return "It's a draw!";
+    }
+
+}

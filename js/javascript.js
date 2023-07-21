99fd2857d9b6e4c86a5a017f0b37d449c87262e6
Corey Fulop <corey.fulop@gmail.com>
2023-07-21 21:17:45 +0100

    Create game function. Change return value of playRound function to simple string.
    game function calls playRound inside a loop to generate a five round game, report the outcomes of rounds, and the game itself. Return value of playRound simplified to allow game function to keep track of round outcomes and tally scores.

--- js/javascript.js
+++ js/javascript.js
@@ -7,15 +7,50 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    const playerSelectionTitleCase = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let playerSelectionTitleCase = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     if ((playerSelectionTitleCase == "Rock" || computerSelection == "Rock") && (playerSelectionTitleCase == "Scissors" || computerSelection == "Scissors")) {
-        return (playerSelectionTitleCase == "Rock") ? "You win! Rock beats Scissors" : "You lose! Rock beats Scissors";
+        return (playerSelectionTitleCase == "Rock") ? "win" : "lose";
     } else if ((playerSelectionTitleCase == "Scissors" || computerSelection == "Scissors") && (playerSelectionTitleCase == "Paper" || computerSelection == "Paper")) {
-        return (playerSelectionTitleCase == "Scissors") ? "You win! Scissors beats Paper" : "You lose! Scissors beats Paper";
+        return (playerSelectionTitleCase == "Scissors") ? "win" : "lose";
     } else if ((playerSelectionTitleCase == "Paper" || computerSelection == "Paper") && (playerSelectionTitleCase == "Rock" || computerSelection == "Rock")) {
-        return (playerSelectionTitleCase == "Paper") ? "You win! Paper beats Rock" : "You lose! Paper beats Rock";
+        return (playerSelectionTitleCase == "Paper") ? "win" : "lose";
     } else {
-        return "It's a draw!";
+        return "draw";
     }
 
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Choose your shape by typing 'Rock', 'Paper', or 'Scissors'");
+        let computerChoice = getComputerChoice();
+        let roundOutcome = playRound(playerChoice, computerChoice);
+        switch (roundOutcome) {
+            case "win":
+                playerScore += 1;
+                console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
+                break;
+            case "lose":
+                computerScore += 1;
+                console.log(`You lose! ${computerChoice} beats ${playerChoice}.`);
+                break;
+            case "draw":
+                console.log(`It's a draw! You both played ${playerChoice}.`)
+                break;
+        }
+        console.log(`The current scores are Player: ${playerScore}, Computer: ${computerScore}.`);
+        
+    }
+    if (playerScore > computerScore) {
+        console.log(`Congratulations, you won! ${playerScore} - ${computerScore}.`);
+    } else if (playerScore < computerScore) {
+        console.log(`Sorry, you lost! ${playerScore} - ${computerScore}.`)
+    } else {
+        console.log(`It's a draw! ${playerScore} - ${computerScore}.`)
+    }
+
+}
+
+game();

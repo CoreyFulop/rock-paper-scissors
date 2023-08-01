59d7571ea5df59f225283dbd4cd975c877c0abff
Corey Fulop <corey.fulop@gmail.com>
2023-08-01 17:49:55 +0100

    Display running score, announce winner at end of game
    Add styles to make more visually appealing
    
    Game announces winner, and disables buttons so that the game logic doesn't break. CSS styles add some slight visual flair

--- js/javascript.js
+++ js/javascript.js
@@ -1,84 +1,97 @@
 "use strict";
 
+let playerScore = 0;
+let computerScore = 0;
+let maxPoints = 5;
+
 const shapes = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
     return shapes[Math.floor(Math.random() * 3)];
 }
 
-function createRoundResults(playerSelection, winningSelection, losingSelection) {
-    switch (playerSelection) {
+function disableButtons() {
+    document.getElementById("Rock").disabled = true;
+    document.getElementById("Paper").disabled = true;
+    document.getElementById("Scissors").disabled = true;
+}
+
+function checkWinLossCondition(playerScore, computerScore, maxPoints) {
+    if (playerScore == maxPoints) {
+        let gameWinningStatement = document.createElement("p");
+        gameWinningStatement.textContent = `You've won the game!`;
+        gameWinningStatement.style.color = "green"
+        resultsContainer.insertBefore(gameWinningStatement, resultsContainer.firstElementChild);
+        disableButtons();
+    } else if (computerScore == maxPoints) {
+        let gameLosingStatement = document.createElement("p");
+        gameLosingStatement.textContent = `You've lost the game!`;
+        gameLosingStatement.style.color = "red";
+        resultsContainer.insertBefore(gameLosingStatement, resultsContainer.firstElementChild);
+        disableButtons();
+    }
+}
+
+function createRoundResults(playerChoice, winningSelection, losingSelection) {
+    switch (playerChoice) {
         case winningSelection:
             let winningStatement = document.createElement("p");
             winningStatement.textContent = `You win! ${winningSelection} beats ${losingSelection}.`
             resultsContainer.insertBefore(winningStatement, resultsContainer.firstElementChild);
+            playerScore += 1;
+            let playerWinningScore = document.createElement("p");
+            playerWinningScore.textContent = `${playerScore}`;
+            playerScoresContainer.insertBefore(playerWinningScore, playerScoresContainer.firstElementChild);
+            let computerLosingScore = document.createElement("p");
+            computerLosingScore.textContent = `${computerScore}`;
+            computerScoresContainer.insertBefore(computerLosingScore, computerScoresContainer.firstElementChild);
             break;
         case losingSelection:
             let losingStatement = document.createElement("p");
             losingStatement.textContent = `You lose! ${winningSelection} beats ${losingSelection}.`
             resultsContainer.insertBefore(losingStatement, resultsContainer.firstElementChild);
+            computerScore += 1;
+            let playerLosingScore = document.createElement("p");
+            playerLosingScore.textContent = `${playerScore}`;
+            playerScoresContainer.insertBefore(playerLosingScore, playerScoresContainer.firstElementChild);
+            let computerWinningScore = document.createElement("p");
+            computerWinningScore.textContent = `${computerScore}`;
+            computerScoresContainer.insertBefore(computerWinningScore, computerScoresContainer.firstElementChild);
             break;
         default:
             let drawStatement = document.createElement("p");
-            drawStatement.textContent = "It's a draw.";
+            drawStatement.textContent = `It's a draw. Computer played ${playerChoice}.`;
             resultsContainer.insertBefore(drawStatement, resultsContainer.firstElementChild);
+            let playerDrawScore = document.createElement("p");
+            playerDrawScore.textContent = `${playerScore}`
+            playerScoresContainer.insertBefore(playerDrawScore, playerScoresContainer.firstElementChild);
+            let computerDrawScore = document.createElement("p");
+            computerDrawScore.textContent = `${computerScore}`;
+            computerScoresContainer.insertBefore(computerDrawScore, computerScoresContainer.firstElementChild);
             break;
     }
 }
 
 function playRound(e) {
-    let playerSelection = e.target.id;
-    let computerSelection = getComputerChoice();
-    if ((playerSelection == "Rock" || computerSelection == "Rock") && (playerSelection == "Scissors" || computerSelection == "Scissors")) {
-        createRoundResults(playerSelection, "Rock", "Scissors");
-    } else if ((playerSelection == "Scissors" || computerSelection == "Scissors") && (playerSelection == "Paper" || computerSelection == "Paper")) {
-        createRoundResults(playerSelection, "Scissors", "Paper");    
-    } else if ((playerSelection == "Paper" || computerSelection == "Paper") && (playerSelection == "Rock" || computerSelection == "Rock")) {
-        createRoundResults(playerSelection, "Paper", "Rock"); 
+    let playerChoice = e.target.id;
+    let computerChoice = getComputerChoice();
+    if ((playerChoice == "Rock" || computerChoice == "Rock") && (playerChoice == "Scissors" || computerChoice == "Scissors")) {
+        createRoundResults(playerChoice, "Rock", "Scissors");
+    } else if ((playerChoice == "Scissors" || computerChoice == "Scissors") && (playerChoice == "Paper" || computerChoice == "Paper")) {
+        createRoundResults(playerChoice, "Scissors", "Paper");    
+    } else if ((playerChoice == "Paper" || computerChoice == "Paper") && (playerChoice == "Rock" || computerChoice == "Rock")) {
+        createRoundResults(playerChoice, "Paper", "Rock"); 
     } else {
-        createRoundResults(playerSelection); 
+        createRoundResults(playerChoice); 
     }
-
+    checkWinLossCondition(playerScore, computerScore, maxPoints);
 }
+
+alert("Play Rock-Paper-Scissors against the computer. First to 5 points wins!");
+
+const playerScoresContainer = document.querySelector(".player-scores-container");
+const resultsContainer = document.querySelector(".round-results-container");
+const computerScoresContainer = document.querySelector(".computer-scores-container");
 
 const playerButtons = Array.from(document.querySelectorAll(".player-button"));
 playerButtons.forEach(playerButton => playerButton.addEventListener("click", playRound));
-
-const resultsContainer = document.querySelector(".results-container");
-
-/*
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Choose your shape by typing 'Rock', 'Paper', or 'Scissors'");
-        let computerChoice = getComputerChoice();
-        let roundOutcome = playRound(playerChoice, computerChoice);
-        switch (roundOutcome) {
-            case "win":
-                playerScore += 1;
-                console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
-                break;
-            case "lose":
-                computerScore += 1;
-                console.log(`You lose! ${computerChoice} beats ${playerChoice}.`);
-                break;
-            case "draw":
-                console.log(`It's a draw! You both played ${playerChoice}.`)
-                break;
-        }
-        console.log(`The current scores are Player: ${playerScore}, Computer: ${computerScore}.`);
-        
-    }
-    if (playerScore > computerScore) {
-        console.log(`Congratulations, you won! ${playerScore} - ${computerScore}.`);
-    } else if (playerScore < computerScore) {
-        console.log(`Sorry, you lost! ${playerScore} - ${computerScore}.`)
-    } else {
-        console.log(`It's a draw! ${playerScore} - ${computerScore}.`)
-    }
-
-}
-
-game();
-*/
